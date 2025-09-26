0c08461940c70f97beecdc3c17e4442f866a17b0
Sakura Ryoko <sakura@ryoko.soy>
2025-09-26 12:18:54 -0400

    update Schema

--- src/main/java/fi/dy/masa/malilib/util/data/Schema.java
+++ src/main/java/fi/dy/masa/malilib/util/data/Schema.java
@@ -22,8 +22,9 @@ public enum Schema implements IEnumCodecProvider
     // TODO --> Add Schema Versions to this as versions get released
     // Minecraft Data Versions
     SCHEMA_FUTURE  (9999, "FUTURE"),
-	SCHEMA_25W35A  (4542, "25w35a"),
-	SCHEMA_25W31A  (4534, "25w31a"),
+    SCHEMA_1_21_09 (4553, "1.21.9-rc1"),
+    SCHEMA_25W36B  (4546, "25w36b"),
+    SCHEMA_25W31A  (4534, "25w31a"),
     SCHEMA_1_21_08 (4440, "1.21.8"),
     SCHEMA_1_21_07 (4438, "1.21.7"),
     SCHEMA_1_21_06 (4435, "1.21.6"),
